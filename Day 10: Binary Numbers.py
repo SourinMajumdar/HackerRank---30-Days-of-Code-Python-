4427e9c93814397b425e988f82ceeccb5b6553f3
Sourin Majumdar <71887222+SourinMajumdar@users.noreply.github.com>
2020-10-16 09:44:39 +0530

    Create Day 10: Binary Numbers.py

---
+++ Day 10: Binary Numbers.py
@@ -0,0 +1,13 @@
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    def func(num):
+        return num[2:]
+    
+n = int(input().strip())
+a = max(func(bin(n)).split('0')).count('1')
+print(a)
