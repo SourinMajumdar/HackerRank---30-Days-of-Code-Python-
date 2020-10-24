dfcf41af593e1f6baf3c72fc4d8697c7260dbaa3
Sourin Majumdar <71887222+SourinMajumdar@users.noreply.github.com>
2020-10-24 11:12:37 +0530

    Create Day 20: Sorting .py

---
+++ Day 20: Sorting .py
@@ -0,0 +1,15 @@
+swaps = 0
+not_solved = True
+
+while not_solved:
+    for i in range(n-1):
+        if a[i] > a[i+1]:
+            swaps += 1
+            a[i],a[i+1] = a[i+1],a[i]
+            break
+        if i == n-2:
+            not_solved = False
+            
+print("Array is sorted in {} swaps.".format(swaps))
+print("First Element:", a[0])
+print("Last Element:", a[-1])
